ced76216ad4d7e638ac7bd7f27d6d1a99e08506d
sj-0722 <135657061+sj-0722@users.noreply.github.com>
2023-06-05 16:32:15 -0500

    Comit inicial

---
+++ Js/main.js
@@ -0,0 +1,6 @@
+function circulo(){
+    let figura = document.getElementById("figura");
+    /*figura.classList.add("circulo"); /*agrega clases al elemento*/
+    figura.classList.toggle("circulo"); 
+}
+
